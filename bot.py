db1d53d826bca82bb1723e3778270e4c98d9c0fb
jake <cjc0024@uah.edu>
2019-03-03 22:00:00 -0600

    remove pylast

--- bot.py
+++ bot.py
@@ -1,52 +1,76 @@
+# -*- coding: utf-8 -*-
+import asyncio
 import discord
 import json
-import pylast
 import random
 import re
+import requests
 import urllib
 
 from discord.ext import commands
+from PIL import Image
+from io import BytesIO
 
 # set config variables
-with open("config.json") as cfg:
+with open("./config/config.json") as cfg:
     config = json.load(cfg)
 
 token = config["token"]
 api_key = config["key"]
-api_secret = config["secret"]
-
-# get pylast network object
-network = pylast.LastFMNetwork(api_key=api_key, api_secret=api_secret)
 
 bot = commands.Bot(command_prefix='.')
 
+#----------FUNCTIONS----------#
+def select_period(period):
+    if period == 'weekly' or period == 'week' or period == 'w':
+        return '7day'
+    elif period == 'monthly' or period == 'month' or period == 'm':
+        return '1month'
+    elif period == 'yearly' or period == 'year' or period == 'y':
+        return '12month'
+    elif period == 'overall' or period == "all" or period == 'a':
+        return 'overall'
+
+
+def select_limit(size):
+    l, w = size.split('x')
+    return str(int(l) * int(w))
+
+
+async def update_now_playing():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        track = requests.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&api_key='+api_key+'&user=networkdrift'+'&limit=1&format=json').json()
+        title = track['recenttracks']['track'][0]['name']
+        artist = track['recenttracks']['track'][0]['artist']['#text']
+        await bot.change_presence(activity=discord.Game(artist + ' – ' + title))
+        await asyncio.sleep(5)
+
+
+#----------EVENTS----------#
 # log in status
 @bot.event
 async def on_ready():
-    print('Logged in as') 
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-    await bot.change_presence(activity=discord.Game("in API hell"))
+    print('logged in.')
 
 @bot.event
-async def on_message(message):  
+async def on_message(message):
     await bot.process_commands(message)
 
-    if message.content.lower().startswith("this is so sad"):
-        await message.channel.send("alexa play despacito")
-
-    if message.content.lower().startswith("ping"):
-        await message.channel.send("pong!")
+    if message.author == bot.user:
+        return
 
     if "john" in message.content.lower():
         emoji = (bot.get_emoji(439200062795415562))
         await message.channel.send(emoji)
 
+
+#----------COMMANDS----------#
 # greet
 @bot.command()
 async def greet(ctx):
     await ctx.send("Hello, world.")
+
 
 # roll dice
 @bot.command()
@@ -57,137 +81,113 @@ async def roll(ctx, r: str):
         multiplier = int(params[0])
         sides = int(params[1])
     except ValueError:
-        await ctx.send("ERROR: invalid params")
-    else:
-        roll = 0
+        await ctx.send("the parameters you sent are invalid.")
+        return
 
+        roll = 0
         for x in range(multiplier):
-            roll += random.randint(1,sides)
+            roll += random.randint(1, sides)
         
         if multiplier > 1:
-            await ctx.send("The dice are cast... " + "**"+str(roll)+"**!")
+            await ctx.send("the dice are cast... " + "**"+str(roll)+"**!")
         else:
-            await ctx.send("The die is cast..." + "**"+str(roll)+"**!") 
+            await ctx.send("the die is cast..." + "**"+str(roll)+"**!")
        
 
 # get current scrobble
-@bot.command()
-async def np(ctx):
-    #open users.json
-    with open('users.json', 'r+') as users:
-        names = json.load(users)
+@bot.command(pass_context=True)
+async def fm(ctx):
+    with open('./config/users.json', 'r+') as u:
+        users = json.load(u)
 
-    sender = names[str(ctx.message.author)]
-    if sender:
-        # data fields 
-        track_title = " "
-        track_album = " "
-        track_artist = " "
-        album_cover = " "
+    try:
+        sender = users[str(ctx.message.author.id)]
+    except KeyError:
+        await ctx.send("you have not supplied your last.fm username. to do so, use .fmset.")
+        return
 
-        user = network.get_user(sender)
-        
-        track = user.get_now_playing()
-        if track == None:
-            track = user.get_recent_tracks(cacheable=False,limit=2)[0]
-            track_title = str(track.track.title)
+    track = requests.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&api_key='+api_key+'&user='+sender+'&limit=1&format=json').json()
 
-            try: 
-                track_album = str(track.album)
-                pass
-            except AttributeError:
-                track_album = "[unknown]"
-                pass
+    title = track['recenttracks']['track'][0]['name']
+    album = track['recenttracks']['track'][0]['album']['#text']
+    artist = track['recenttracks']['track'][0]['artist']['#text']
+    artwork = track['recenttracks']['track'][0]['image'][2]['#text']
 
-            track_artist = str(track.track.artist)
+    # make the embed
+    embed = discord.Embed(description=" ", color=0x535660)
+    embed.add_field(name=title, value="\nfrom " + "*"+album+"*"+"\nby " + artist, inline=True)
+    embed.set_author(name= str(ctx.message.author.display_name)+" on last.fm", url="https://www.last.fm/user/"+sender, icon_url=ctx.message.author.avatar_url)
 
-            try: 
-                album_cover = track.track.get_cover_image(size=1)
-                pass
-            except AttributeError:
-                # todo: make filler cover
-                pass 
-        else:
-            track_title = str(track.title)
+    if artwork:
+        embed.set_thumbnail(url=artwork)
 
-            try: 
-                track_album = str(track.get_album().get_name())
-                pass
-            except AttributeError:
-                track_album = "[unknown]"
-                pass
+    await ctx.send(embed=embed)
 
-            track_artist = str(track.artist)
+# generate a chart of the user's top albums
+@bot.command(pass_context=True)
+async def fmchart(ctx, period="weekly", size="3x3"):
+    with open('./config/users.json', 'r+') as u:
+        users = json.load(u)
 
-            try: 
-                album_cover = track.get_album().get_cover_image(size=1)
-                pass
-            except AttributeError:
-                # todo: make filler cover
-                pass 
+    sender = users[str(ctx.message.author.id)]
+    albums = requests.get('http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&api_key='+api_key+'&user='+sender+'&limit='+select_limit(size)+'&period='+select_period(period)+'&format=json').json()
 
-        # make the embed
-        embed = discord.Embed(title="Now Playing",description=" ", color=0x535660)
-        embed.add_field(name=track_title,value="\nfrom " + "*"+track_album+"*"+"\nby " + track_artist,inline=True)
+    per_row = int(size[:size.find('x')])
+    per_col = int(size[size.find('x')+1:])
+    canvas = Image.new('RGB', (per_row*300,per_col*300))
 
-        if album_cover:
-            embed.set_thumbnail(url=album_cover)                
+    covers = [album['image'][3]['#text'] for album in albums['topalbums']['album']]
+    offset_x = 0
+    offset_y = 0
+    for cover in covers:
+        if offset_x == canvas.width:
+            offset_x = 0
+            offset_y += 300
+        try:
+            image = Image.open(BytesIO(requests.get(cover).content))
+            canvas.paste(image, (offset_x, offset_y))
+        except requests.exceptions.MissingSchema:
+            pass
+        offset_x += 300
 
-        await ctx.send(embed=embed)       
-    else:
-        await ctx.send("No valid username found. To set your username, use .npset")
+        if canvas.width > 1500:
+            canvas.resize((1500, 1500))
+
+    buffer = BytesIO()
+    canvas.save(buffer, format='jpeg', quality=100)
+    buffer = buffer.getvalue()
+    await ctx.send('', file=discord.File(fp=buffer, filename='chart.jpg'))
+
+    # make the embed
+    embed = discord.Embed(description=" ", color=0x535660)
+    embed.add_field(name=ctx.message.author.display_name+'\'s last.fm chart', value=period+', '+size, inline=True)
+    embed.set_author(name= str(ctx.message.author.display_name)+" on last.fm", url="https://www.last.fm/user/"+sender, icon_url=ctx.message.author.avatar_url)
+    await ctx.send(embed=embed)
+
 
 # set last.fm username
-@bot.command()
-async def npset(ctx):
-    try:
-        user = network.get_user(ctx.message.content[7:])
-        user.get_registered()
-        pass
-    except pylast.WSError:
-        await ctx.send("\nInvalid username.")
-        pass
-    else:
-        with open('users.json', 'r') as users:
-            names = json.load(users)
+@bot.command(pass_context=True)
+async def fmset(ctx, username=None):
 
-        names[str(ctx.message.author)] = str(user)
+    if username is None:
+        await ctx.send("you did not provide a username.")
+        return
 
-        with open("users.json", "w") as users:
-            json.dump(names, users)
+    user = requests.get('http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user='+username+'&api_key='+api_key+'&format=json').json()
 
-        await ctx.send("Username set.")
+    if 'error' in user:
+        await ctx.send('that user doesn\'t exist on last.fm. did you make a typo?')
+        return
 
-@bot.command()
-async def weekly(ctx, t: str):
-    with open('users.json', 'r+') as users:
-        names = json.load(users)
+    with open('./config/users.json', 'r') as u:
+        users = json.load(u)
 
-    sender = names[str(ctx.message.author)]
+    users[str(ctx.message.author.id)] = str(username)
 
-    if sender:
-        user = network.get_user(sender)
+    with open("./config/users.json", "w") as u:
+        json.dump(users, u, indent= 4, sort_keys=True)
 
-        if t == "albums":
-            query = user.get_top_albums(period='7day', limit=5)
-        elif t == "tracks":
-            query = user.get_top_tracks(period='7day', limit=5)
-        elif t == "artists":
-            query = user.get_top_artists(period='7day', limit=5)
-        else:
-            await ctx.send("Invalid parameter.")
-            return
-
-        output = "**Top 5 "+t.capitalize()+" (weekly):**"
-        
-        for x in range(len(query)):
-                output+="\n"+str(x+1)+". "+str(query[x].item)
-
-
-        await ctx.send(output)
-
-    else:
-        await ctx.send("No valid username found. To set your username, use .npset")       
+    await ctx.send('set ' + ctx.message.author.name + '\'s last.fm username to ' + username + '.')
 
 
 # get first result from youtube query
@@ -197,5 +197,14 @@ async def yt(ctx):
     search = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + query)
     results = re.findall(r'href=\"\/watch\?v=(.{11})', search.read().decode())
     await ctx.send("http://www.youtube.com/watch?v=" +  results[0])
+
+
+# log out
+@bot.command()
+# @is_owner()
+async def quit(ctx):
+    await bot.logout()
+
+bot.loop.create_task(update_now_playing())
 
 bot.run(token)
